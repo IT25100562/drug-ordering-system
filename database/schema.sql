ebd71ad01abfe3e00d0b0120dd29f4a8fd90438d
dulithdivisekara <it25100562@my.sliit.lk>
2026-09-17 00:28:22 +0530

    feat: complete browsing and cart module

--- database/schema.sql
+++ database/schema.sql
@@ -25,3 +25,33 @@ CREATE TABLE users (
                        role VARCHAR(20) DEFAULT 'customer', -- customer, pharmacist, admin
                        created_at DATETIME DEFAULT GETDATE()
 );
+
+CREATE TABLE users (
+                       id INT IDENTITY(1,1) PRIMARY KEY,
+                       full_name VARCHAR(100) NOT NULL,
+                       email VARCHAR(100) UNIQUE NOT NULL,
+                       password_hash VARCHAR(255) NOT NULL,
+                       role VARCHAR(20) DEFAULT 'customer',
+                       created_at DATETIME DEFAULT GETDATE()
+);
+
+CREATE TABLE medicines (
+                           id INT IDENTITY(1,1) PRIMARY KEY,
+                           name VARCHAR(150) NOT NULL,
+                           category VARCHAR(100) NOT NULL,
+                           price DECIMAL(10,2) NOT NULL,
+                           stock_quantity INT DEFAULT 0,
+                           description VARCHAR(MAX),
+    is_discontinued BIT DEFAULT 0,
+    created_at DATETIME DEFAULT GETDATE()
+);
+
+CREATE TABLE cart_items (
+                            id INT IDENTITY(1,1) PRIMARY KEY,
+                            user_id INT NOT NULL,
+                            medicine_id INT NOT NULL,
+                            quantity INT DEFAULT 1,
+                            added_at DATETIME DEFAULT GETDATE(),
+                            FOREIGN KEY (user_id) REFERENCES users(id),
+                            FOREIGN KEY (medicine_id) REFERENCES medicines(id)
+);
